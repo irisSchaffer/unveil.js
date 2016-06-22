8b02f104ded8c7cdd777d7454f487b63b9d5d309
Iris Schaffer <irisSchaffer@gmx.net>
2016-06-22 21:06:28 +0200

    Finally fix subsliding problem!

--- src/components/UnveilApp.js
+++ src/components/UnveilApp.js
@@ -1,19 +1,21 @@
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs'
 
-import React from 'react';
+import React from 'react'
 
-import Slide     from './Slide';
-import Presenter from './Presenter';
+import { cloneDeep } from 'lodash'
 
-import KeyControls from './KeyControls';
-import UIControls  from './UIControls';
+import Slide     from './Slide'
+import Presenter from './Presenter'
 
-import getDirections   from '../getDirections';
-import createRouter    from './Router';
-import createNavigator from './Navigator';
-import history         from '../helpers/History';
+import KeyControls from './KeyControls'
+import UIControls  from './UIControls'
 
-import '../lib/Utils';
+import getDirections   from '../getDirections'
+import createRouter    from './Router'
+import createNavigator from './Navigator'
+import history         from '../helpers/History'
+
+import '../lib/Utils'
 
 export default React.createClass({
 
@@ -26,47 +28,47 @@ export default React.createClass({
         let entry = {
           index,
           name: slide.props.name || false
-        };
+        }
 
         if(this.areSlides(slide.props.children))
-          entry.children = this.buildMap(slide.props.children.toList());
+          entry.children = this.buildMap(slide.props.children.toList())
 
-        return entry;
+        return entry
       }
-    }).compact();
+    }).compact()
   },
 
   componentWillMount: function () {
-    let controls  = this.props.controls || [UIControls, KeyControls];
-    let presenter = this.props.presenter || Presenter;
+    let controls  = this.props.controls || [UIControls, KeyControls]
+    let presenter = this.props.presenter || Presenter
 
     this.modes    = this.props.modes || {
       default: {
         controls : controls,
         presenter: presenter
       }
-    };
+    }
 
-    this.stateSubject   = new Subject();
-    this.history        = this.props.history || history;
-    this.slides         = this.autoNameSlides(this.props.children);
-    this.getDirections  = this.props.getDirections || getDirections;
-    this.routerState    = { directions: [], query: {} };
+    this.stateSubject   = new Subject()
+    this.history        = this.props.history || history
+    this.slides         = this.autoNameSlides(this.props.children)
+    this.getDirections  = this.props.getDirections || getDirections
+    this.routerState    = { directions: [], query: {} }
 
-    this.setup();
+    this.setup()
 
     this.stateSubject
       .filter(this.isEventOfType('state/slide:add'))
       .map(this.newSlide)
-      .subscribe(this.addSlide);
+      .subscribe(this.addSlide)
 
     this.stateSubject
       .filter(this.isEventOfType('state/navigation:enable'))
-      .subscribe(() => this.setState({navigatable: true}));
+      .subscribe(() => this.setState({navigatable: true}))
 
     this.stateSubject
       .filter(this.isEventOfType('state/navigation:disable'))
-      .subscribe(() => this.setState({navigatable: false}));
+      .subscribe(() => this.setState({navigatable: false}))
   },
 
   childContextTypes: {
@@ -88,58 +90,58 @@ export default React.createClass({
       slide:        this.getSlide(this.routerState.indices),
       stateSubject: this.stateSubject,
       history:      this.history
-    };
+    }
   },
 
   autoNameSlides: function (children, index = '') {
-    if (!Array.isArray(children) || children.length === 0) return children;
+    if (!Array.isArray(children) || children.length === 0) return children
     return children.toList()
       .map((s, i) => {
         if (!Slide.isSlide(s)) {
           return s
         }
 
-        let name = index + (s.props.name || i);
+        let name = index + (s.props.name || i)
         return React.cloneElement(
           s, { name: name, key: name },
           this.autoNameSlides(s.props.children, (name + '-'))
-        );
-      });
+        )
+      })
   },
 
   setup: function () {
-    this.map = this.buildMap(this.slides);
+    this.map = this.buildMap(this.slides)
 
     this.router = createRouter({
       map: this.map,
       history: this.history,
       getDirections: this.getDirections
-    });
+    })
 
     this.navigator = (this.props.navigator || createNavigator)({
       stateObservable: this.router.asObservable().map((routerState) => ({...routerState, slide: this.getSlide(routerState.indices)})),
       history:         this.history
-    });
+    })
 
     this.routerUnsubscribe = this.router.asObservable()
-      .subscribe(this.updateState);
+      .subscribe(this.updateState)
 
     this.navigatorUnsubscribe = this.navigator.asObservable()
       .subscribe(function(e) {
         this.router.go(e, this.routerState.query)
-      }.bind(this));
+      }.bind(this))
 
-    this.router.start();
+    this.router.start()
   },
 
   tearDown: function () {
-    this.navigatorUnsubscribe.unsubscribe();
-    this.routerUnsubscribe.unsubscribe();
-    this.router.stop();
+    this.navigatorUnsubscribe.unsubscribe()
+    this.routerUnsubscribe.unsubscribe()
+    this.router.stop()
   },
 
   getMode: function () {
-    return this.modes[this.state.mode] || this.modes['default'];
+    return this.modes[this.state.mode] || this.modes['default']
   },
 
   isEventOfType: function(type) {
@@ -152,7 +154,7 @@ export default React.createClass({
     return {
       ...data,
       slide: React.createElement(Slide, {}, data.content),
-    };
+    }
   },
 
   addSubslide: function (target, newSlide) {
@@ -163,15 +165,19 @@ export default React.createClass({
       children.push(newSlide)
       parentSlide = React.cloneElement(parentSlide, {}, children)
     } else {
-      let newSubslide = React.createElement(Slide, {id: undefined, name: undefined}, parentSlide.props.children)
-      parentSlide = React.createElement(Slide, {id: parentSlide.props.id, name:parentSlide.props.name}, [newSubslide, newSlide])
+      let newSubslide = React.createElement(
+        Slide,
+        {id: `${parentSlide.props.id}-0`, name: `0`},
+        cloneDeep(parentSlide.props.children)
+      )
+      parentSlide = React.createElement(Slide, {id: parentSlide.props.id, name: parentSlide.props.name}, [newSubslide, newSlide])
     }
 
     this.slides.splice(target[0], 1, parentSlide)
   },
 
   addSlide: function (data) {
-    const i = data.location.indices;
+    const i = data.location.indices
 
     switch(data.method) {
       case 'after':
@@ -192,41 +198,41 @@ export default React.createClass({
     return {
       mode:         'default',
       navigatable:  true
-    };
+    }
   },
 
   updateState: function (s) {
-    this.routerState = s;
-    this.setState({ mode: s.query.mode || 'default' });
+    this.routerState = s
+    this.setState({ mode: s.query.mode || 'default' })
   },
 
   areSlides: function (children) {
     return children.toList()
       .map(Slide.isSlide)
-      .reduce((a,b) => (a&&b), true);
+      .reduce((a,b) => (a&&b), true)
   },
 
   getSlide: function (indices) {
-    let slide = this.slides.toList()[indices[0]];
+    let slide = this.slides.toList()[indices[0]]
     if (indices.length === 1 && this.areSlides(slide.props.children)) {
-      indices[1] = 0;
+      indices[1] = 0
     }
     if(indices.length > 1)
-      return slide.props.children.toList()[indices[1]];
+      return slide.props.children.toList()[indices[1]]
     else
       return slide
   },
 
 
   presenterElement: function() {
-    let mode = this.getMode();
+    let mode = this.getMode()
     return React.createElement(
       mode.presenter,
       {
         unveil: this,
         controls: mode.controls
       }
-    );
+    )
   },
 
   render: function () {
@@ -234,7 +240,7 @@ export default React.createClass({
       <div>
         {this.presenterElement()}
       </div>
-    );
+    )
   }
 
-});
+})
